67617c9efe25abdd7b7c3f61a533fac74c70a718
vcapp <vcappuccio@gmail.com>
2024-09-01 18:07:13 +0200

    pass stage

--- app/main.ts
+++ app/main.ts
@@ -9,254 +9,159 @@ const [, , , resolverArg] = argv;
 const [resolverIp, resolverPort] = resolverArg.split(':');
 const RESOLVER_PORT = parseInt(resolverPort, 10);
 
-type Question = {
-  domain: string;
-  qType: number;
-  qClass: number;
-};
+interface Question {
+  name: string;
+  qtype: number;
+  qclass: number;
+}
 
-type DNSRecord = Question & {
+interface DNSRecord {
+  name: string;
+  type: number;
+  cls: number;
   ttl: number;
-  data: string;
-};
-
-type Label = {
-  offset: number;
-  content: string;
-  next?: Label;
-};
-
-function followOffsets({
-  labels,
-  labelOffsetMap,
-  offset,
-}: {
-  labels: Label[];
-  labelOffsetMap: Record<number, Label>;
-  offset: number;
-}) {
-  let currentLabel: Label | undefined = labelOffsetMap[offset];
-  while (currentLabel) {
-    labels.push(currentLabel);
-    currentLabel = currentLabel.next;
-  }
-}
-
-function parseQuestionSection(questionSection: Buffer, offset: number): Question[] {
-  let i = offset;
-  const questions: Question[] = [];
-  
-  while (i < questionSection.length) {
-    const domain = parseDomainName(questionSection, i);
-    i += domain.byteLength;
-    
-    if (i + 4 > questionSection.length) {
-      break;
-    }
-    
-    const qType = questionSection.readUInt16BE(i);
-    i += 2;
-    const qClass = questionSection.readUInt16BE(i);
-    i += 2;
-    
-    questions.push({ domain: domain.name, qType, qClass });
-  }
-  
-  return questions;
-}
-
-function parseDomainName(buffer: Buffer, offset: number): { name: string; byteLength: number } {
-  const labels: string[] = [];
-  let i = offset;
-  let byteLength = 0;
-  const maxLength = buffer.length;
-
-  while (i < maxLength) {
-    const length = buffer[i];
-
-    if (length === 0) {
-      byteLength++;
-      break;
-    }
-
-    if ((length & 0xc0) === 0xc0) {
-      if (i + 1 >= maxLength) {
-        throw new Error("Buffer overflow while parsing compressed domain name");
-      }
-      const pointerOffset = ((length & 0x3f) << 8) | buffer[i + 1];
-      const pointerResult = parseDomainName(buffer, pointerOffset);
-      labels.push(...pointerResult.name.split('.'));
-      byteLength += 2;
-      break;
-    }
-
-    i++;
-    byteLength += length + 1;
-
-    if (i + length > maxLength) {
-      throw new Error("Buffer overflow while parsing domain name");
-    }
-
-    labels.push(buffer.slice(i, i + length).toString('ascii'));
-    i += length;
-  }
-
-  return { name: labels.join('.'), byteLength };
-}
-
-function recordToQuestion(record: DNSRecord): Buffer {
-  const domainParts = record.domain.split('.');
-  const domainBuffer = Buffer.concat(
-    domainParts.map(part => Buffer.concat([Buffer.from([part.length]), Buffer.from(part)]))
-  );
-  const endDomain = Buffer.from([0]);
-  const typeAndClass = Buffer.alloc(4);
-  typeAndClass.writeUInt16BE(record.qType, 0);
-  typeAndClass.writeUInt16BE(record.qClass, 2);
-  
-  return Buffer.concat([domainBuffer, endDomain, typeAndClass]);
-}
-
-function recordToAnswer(record: DNSRecord): Buffer {
-  const question = recordToQuestion(record);
-  const ttlAndLength = Buffer.alloc(6);
-  ttlAndLength.writeUInt32BE(record.ttl, 0);
-  const ipParts = record.data.split('.').map(Number);
-  const ipBuffer = Buffer.from(ipParts);
-  ttlAndLength.writeUInt16BE(ipBuffer.length, 4);
-  
-  return Buffer.concat([question, ttlAndLength, ipBuffer]);
+  rdlength: number;
+  rdata: Buffer;
 }
 
 class DNSMessage {
-  private packetId: number;
-  private queryResponse: boolean;
-  private opCode: number;
-  private authoritativeAnswer: boolean = false;
-  private truncation: boolean = false;
-  private recursionDesired: boolean;
-  private recursionAvailable: boolean = false;
-  private responseCode: number = 0;
-  private questions: Question[] = [];
-  private answers: DNSRecord[] = [];
+  packetId: number = 0;
+  flags: number = 0;
+  questions: Question[] = [];
+  answers: DNSRecord[] = [];
 
   constructor(data?: Buffer) {
     if (data) {
-      this.packetId = data.readUInt16BE(0);
-      const flags = data.readUInt16BE(2);
-      this.queryResponse = Boolean(flags & 0x8000);
-      this.opCode = (flags >> 11) & 0xF;
-      this.authoritativeAnswer = Boolean(flags & 0x0400);
-      this.truncation = Boolean(flags & 0x0200);
-      this.recursionDesired = Boolean(flags & 0x0100);
-      this.recursionAvailable = Boolean(flags & 0x0080);
-      this.responseCode = flags & 0x000F;
-      
-      const questionCount = data.readUInt16BE(4);
-      const answerCount = data.readUInt16BE(6);
-      
-      let offset = 12;
-      for (let i = 0; i < questionCount; i++) {
-        const [question, newOffset] = this.parseQuestion(data, offset);
-        this.questions.push(question);
-        offset = newOffset;
-      }
-      
-      for (let i = 0; i < answerCount; i++) {
-        const [answer, newOffset] = this.parseAnswer(data, offset);
-        this.answers.push(answer);
-        offset = newOffset;
-      }
-    } else {
-      this.packetId = 0;
-      this.queryResponse = false;
-      this.opCode = 0;
-      this.recursionDesired = false;
+      this.parse(data);
+    }
+  }
+
+  parse(data: Buffer): void {
+    let offset = 0;
+    this.packetId = data.readUInt16BE(offset);
+    offset += 2;
+    this.flags = data.readUInt16BE(offset);
+    offset += 2;
+    const qdcount = data.readUInt16BE(offset);
+    offset += 2;
+    const ancount = data.readUInt16BE(offset);
+    offset += 2;
+    // Skip NSCOUNT and ARCOUNT
+    offset += 4;
+
+    for (let i = 0; i < qdcount; i++) {
+      const [question, newOffset] = this.parseQuestion(data, offset);
+      this.questions.push(question);
+      offset = newOffset;
+    }
+
+    for (let i = 0; i < ancount; i++) {
+      const [answer, newOffset] = this.parseAnswer(data, offset);
+      this.answers.push(answer);
+      offset = newOffset;
     }
   }
 
   private parseQuestion(data: Buffer, offset: number): [Question, number] {
-    const { name, byteLength } = parseDomainName(data, offset);
-    offset += byteLength;
-    
-    if (offset + 4 > data.length) {
-      throw new Error("Buffer overflow while parsing question");
-    }
-    
-    const qType = data.readUInt16BE(offset);
+    const [name, newOffset] = this.parseName(data, offset);
+    offset = newOffset;
+    const qtype = data.readUInt16BE(offset);
     offset += 2;
-    const qClass = data.readUInt16BE(offset);
+    const qclass = data.readUInt16BE(offset);
     offset += 2;
-    return [{ domain: name, qType, qClass }, offset];
+    return [{ name, qtype, qclass }, offset];
   }
 
   private parseAnswer(data: Buffer, offset: number): [DNSRecord, number] {
-    const { name, byteLength } = parseDomainName(data, offset);
-    offset += byteLength;
-    
-    if (offset + 10 > data.length) {
-      throw new Error("Buffer overflow while parsing answer");
-    }
-    
-    const qType = data.readUInt16BE(offset);
+    const [name, newOffset] = this.parseName(data, offset);
+    offset = newOffset;
+    const type = data.readUInt16BE(offset);
     offset += 2;
-    const qClass = data.readUInt16BE(offset);
+    const cls = data.readUInt16BE(offset);
     offset += 2;
     const ttl = data.readUInt32BE(offset);
     offset += 4;
-    const dataLength = data.readUInt16BE(offset);
+    const rdlength = data.readUInt16BE(offset);
     offset += 2;
-    
-    if (offset + dataLength > data.length) {
-      throw new Error("Buffer overflow while parsing answer data");
+    const rdata = data.slice(offset, offset + rdlength);
+    offset += rdlength;
+    return [{ name, type, cls, ttl, rdlength, rdata }, offset];
+  }
+
+  private parseName(data: Buffer, offset: number): [string, number] {
+    const labels: string[] = [];
+    let jumping = false;
+    let jumpOffset = -1;
+
+    while (true) {
+      const length = data[offset];
+      if (length === 0) {
+        if (!jumping) offset++;
+        break;
+      }
+      if ((length & 0xC0) === 0xC0) {
+        if (!jumping) {
+          jumpOffset = offset + 2;
+        }
+        offset = ((length & 0x3F) << 8) | data[offset + 1];
+        jumping = true;
+        continue;
+      }
+      offset++;
+      labels.push(data.slice(offset, offset + length).toString('ascii'));
+      offset += length;
+      if (jumping && jumpOffset !== -1) {
+        offset = jumpOffset;
+        jumping = false;
+        jumpOffset = -1;
+      }
     }
-    
-    const recordData = data.slice(offset, offset + dataLength).toString('utf-8');
-    offset += dataLength;
-    return [{ domain: name, qType, qClass, ttl, data: recordData }, offset];
+
+    return [labels.join('.'), offset];
   }
 
   toBuffer(): Buffer {
-    const header = this.getHeader();
-    const questions = this.getQuestionSection();
-    const answers = this.getAnswerSection();
+    const headerBuffer = Buffer.alloc(12);
+    headerBuffer.writeUInt16BE(this.packetId, 0);
+    headerBuffer.writeUInt16BE(this.flags, 2);
+    headerBuffer.writeUInt16BE(this.questions.length, 4);
+    headerBuffer.writeUInt16BE(this.answers.length, 6);
+    headerBuffer.writeUInt16BE(0, 8); // NSCOUNT
+    headerBuffer.writeUInt16BE(0, 10); // ARCOUNT
 
-    return Buffer.concat([header, questions, answers]);
+    const questionBuffers = this.questions.map(q => this.questionToBuffer(q));
+    const answerBuffers = this.answers.map(a => this.answerToBuffer(a));
+
+    return Buffer.concat([headerBuffer, ...questionBuffers, ...answerBuffers]);
   }
 
-  private getHeader(): Buffer {
-    const header = Buffer.alloc(12);
-    header.writeUInt16BE(this.packetId, 0);
-    
-    let flags = 0;
-    flags |= this.queryResponse ? 0x8000 : 0;
-    flags |= (this.opCode << 11) & 0x7800;
-    flags |= this.authoritativeAnswer ? 0x0400 : 0;
-    flags |= this.truncation ? 0x0200 : 0;
-    flags |= this.recursionDesired ? 0x0100 : 0;
-    flags |= this.recursionAvailable ? 0x0080 : 0;
-    flags |= this.responseCode & 0x000F;
-    
-    header.writeUInt16BE(flags, 2);
-    header.writeUInt16BE(this.questions.length, 4); // QDCOUNT
-    header.writeUInt16BE(this.answers.length, 6); // ANCOUNT
-    header.writeUInt16BE(0, 8); // NSCOUNT
-    header.writeUInt16BE(0, 10); // ARCOUNT
-
-    return header;
+  private questionToBuffer(question: Question): Buffer {
+    const nameBuffer = this.nameToBuffer(question.name);
+    const typeClassBuffer = Buffer.alloc(4);
+    typeClassBuffer.writeUInt16BE(question.qtype, 0);
+    typeClassBuffer.writeUInt16BE(question.qclass, 2);
+    return Buffer.concat([nameBuffer, typeClassBuffer]);
   }
 
-  private getQuestionSection(): Buffer {
-    return Buffer.concat(this.questions.map(recordToQuestion));
+  private answerToBuffer(answer: DNSRecord): Buffer {
+    const nameBuffer = this.nameToBuffer(answer.name);
+    const fixedBuffer = Buffer.alloc(10);
+    fixedBuffer.writeUInt16BE(answer.type, 0);
+    fixedBuffer.writeUInt16BE(answer.cls, 2);
+    fixedBuffer.writeUInt32BE(answer.ttl, 4);
+    fixedBuffer.writeUInt16BE(answer.rdlength, 8);
+    return Buffer.concat([nameBuffer, fixedBuffer, answer.rdata]);
   }
 
-  private getAnswerSection(): Buffer {
-    return Buffer.concat(this.answers.map(recordToAnswer));
-  }
-
-  setAnswers(answers: DNSRecord[]) {
-    this.answers = answers;
-    this.queryResponse = true;
+  private nameToBuffer(name: string): Buffer {
+    const parts = name.split('.');
+    const buffers = parts.map(part => {
+      const buffer = Buffer.alloc(part.length + 1);
+      buffer.writeUInt8(part.length, 0);
+      buffer.write(part, 1);
+      return buffer;
+    });
+    return Buffer.concat([...buffers, Buffer.from([0])]);
   }
 }
 
@@ -283,41 +188,6 @@ function forwardDNSQuery(query: Buffer): Promise<Buffer> {
   });
 }
 
-class DNS {
-  parseQuestion(data: Buffer, startOffset: number): [string, number] {
-    let labels: string[] = [];
-    let offset = startOffset;
-    let jumping = false;
-    let jumpOffset = -1;
-
-    while (offset < data.length) {
-      const length = data[offset];
-      if (length === 0) {
-        if (!jumping) offset++;
-        break;
-      }
-      if ((length & 0xC0) === 0xC0) {
-        if (!jumping) {
-          jumpOffset = offset + 2;
-        }
-        jumping = true;
-        offset = ((length & 0x3F) << 8) | data[offset + 1];
-        continue;
-      }
-      if (offset + length + 1 > data.length) break;
-      labels.push(data.subarray(offset + 1, offset + 1 + length).toString("ascii"));
-      offset += length + 1;
-      if (jumping && jumpOffset !== -1) {
-        offset = jumpOffset;
-        jumping = false;
-        jumpOffset = -1;
-      }
-    }
-
-    return [labels.join('.'), offset];
-  }
-}
-
 udpSocket.on('message', async (data: Buffer, remoteAddr: dgram.RemoteInfo) => {
   try {
     const query = new DNSMessage(data);
@@ -330,9 +200,8 @@ udpSocket.on('message', async (data: Buffer, remoteAddr: dgram.RemoteInfo) => {
     console.error(`Error processing or sending data: ${e}`);
     // Send an error response to the client
     const errorResponse = new DNSMessage();
-    errorResponse.packetId = data.readUInt16BE(0); // Use the original query ID
-    errorResponse.queryResponse = true;
-    errorResponse.responseCode = 2; // Server failure
+    errorResponse.packetId = data.readUInt16BE(0);
+    errorResponse.flags = 0x8182; // Response + Server Failure
     const errorBuffer = errorResponse.toBuffer();
     udpSocket.send(errorBuffer, remoteAddr.port, remoteAddr.address);
   }
